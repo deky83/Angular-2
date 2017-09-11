d7ff115ecd21596a1f3308ab54a83d91892ec6a7
Dejan Pavlovic <dejan.pavlovic@creative-brackets.com>
2017-09-11 16:47:10 +0200

    postavka projekta

---
+++ src/app/returns-json-array.service.ts
@@ -0,0 +1,22 @@
+
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ReturnsJsonArrayService {
+
+  constructor(private http: Http) {}
+
+  getPeople(): Observable<any> {
+    /*return this.http.request('./data/currencies.json')
+        .map(res => res.json());*/
+
+        return this.http.get('./assets/data/currencies.json')
+          // ...and calling .json() on the response to return data
+          .map((res:Response) => res.json())
+          //...errors if any
+          .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+}
